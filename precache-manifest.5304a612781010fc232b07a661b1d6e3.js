2980640873c0ec9631aa56fed8781d986edc346c
tmnrwd <61465712+tmnrwd@users.noreply.github.com>
2020-09-09 09:32:45 +0100

    Updates

---
+++ precache-manifest.5304a612781010fc232b07a661b1d6e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f652100434e945f13f7730893c13f93e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "fa2431fb10c1902f1fb0",
+    "url": "/weather-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "52475c2862adc1a952f4",
+    "url": "/weather-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "fa2431fb10c1902f1fb0",
+    "url": "/weather-app/static/js/2.8baff893.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weather-app/static/js/2.8baff893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52475c2862adc1a952f4",
+    "url": "/weather-app/static/js/main.0b8957ca.chunk.js"
+  },
+  {
+    "revision": "600b942c319ec142136f",
+    "url": "/weather-app/static/js/runtime-main.9524ac04.js"
+  }
+]);
